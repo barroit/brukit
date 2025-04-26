barroit/brukit

commit fea0301c498fa1e1c91b7e5f5c72f521c48d755e

pathwalk: rename module name to pathwalk
Signed-off-by: Jiamu Sun <barroit@linux.com>

platfree/pathwalk.c
@@ -1,0 +1,374 @@
+// SPDX-License-Identifier: GPL-3.0-or-later or MIT
+/*
+ * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ */
+
+#include "pathwalk.h"
+
+#include "path.h"
+#include "xalloc.h"
+#include "xc.h"
+#include "xcf.h"
+
+void __pw_init(struct pathwalk *pa, xchar *buf, uint len, xchar *rtb)
+{
+	BUG_ON(buf[-1] != 0);
+	BUG_ON(!len);
+
+	memset(pa, 0, sizeof(*pa));
+
+	pa->buf = buf;
+	pa->len = len;
+	pa->rtb = rtb;
+
+	if (pa->rtb == pa->buf)
+		pa->flags |= PW_SHR_RTB;
+
+	pw_step(pa);
+
+	switch (pa->st) {
+	case PW_ROOT_NAME:
+		pa->root_len = pa->se_len;
+		pw_step(pa);
+
+		if (pa->st != PW_ROOT_DIR)
+			break;
+
+	case PW_ROOT_DIR:
+		pa->root_len += 1;
+		pa->flags |= PW_IS_ABS;
+	}
+
+	pa->st = PW_END;
+}
+
+void pw_init(struct pathwalk *pa, const xchar *path)
+{
+	uint len = xc_strlen(path);
+	uint rsz = (len + 1) * sizeof(*path);
+	uint bsz = (len + 2) * sizeof(*path);
+
+	xchar *buf = xmalloc(bsz);
+	xchar *rtb = xmalloc(rsz);
+
+	buf[0] = 0;
+	memcpy(&buf[1], path, rsz);
+
+	__pw_init(pa, &buf[1], len, rtb);
+}
+
+void pw_destroy(struct pathwalk *pa)
+{
+	if (pa->rtb != pa->buf)
+		free(pa->rtb);
+
+	free(&pa->buf[-1]);
+}
+
+static inline void next_state(struct pathwalk *pa,
+			      enum pathwalk_state st,
+			      const xchar *se, uint len)
+{
+	pa->st = st;
+
+	pa->se = se;
+	pa->se_len = len;
+}
+
+const xchar *pw_skip_sep(const xchar *name)
+{
+	while (chr_is_sep(*name))
+		name++;
+
+	return name;
+}
+
+const xchar *pw_skip_nsep(const xchar *name, uint n)
+{
+	const xchar *p = pw_skip_sep(name);
+
+	if (name + n == p)
+		return p;
+
+	return name;
+}
+
+const xchar *pw_skip_name(const xchar *name)
+{
+	while (*name && !chr_is_sep(*name))
+		name++;
+
+	return name;
+}
+
+const xchar *pw_skip_sep_back(const xchar *name)
+{
+	while (chr_is_sep(*name))
+		name--;
+
+	return name;
+}
+
+const xchar *pw_skip_nsep_back(const xchar *name, uint n)
+{
+	const xchar *p = pw_skip_sep_back(name);
+
+	if (name - n == p)
+		return p;
+
+	return name;
+}
+
+const xchar *pw_skip_name_back(const xchar *name)
+{
+	while (*name && !chr_is_sep(*name))
+		name--;
+
+	const xchar *tmp = pw_skip_root(&name[1]);
+
+	if (tmp != &name[1])
+		name = &tmp[-1];
+	return name;
+}
+
+int pw_step(struct pathwalk *pa)
+{
+	const xchar *prev = NULL;
+	const xchar *next;
+
+	switch (pa->st) {
+	case PW_PRE:
+		prev = pa->buf;
+		next = pw_skip_root(prev);
+
+		if (next != prev) {
+			next_state(pa, PW_ROOT_NAME, prev, next - prev);
+			break;
+		}
+
+	case PW_ROOT_NAME:
+		if (!prev)
+			prev = &pa->se[pa->se_len];
+		next = pw_skip_sep(prev);
+
+		if (next != prev) {
+			next_state(pa, PW_ROOT_DIR, prev, next - prev);
+			break;
+		}
+
+	case PW_ROOT_DIR:
+		if (!prev)
+			prev = &pa->se[pa->se_len];
+		next = pw_skip_name(prev);
+
+		if (next != prev) {
+			next_state(pa, PW_FILE_NAME, prev, next - prev);
+			break;
+		}
+
+		next_state(pa, PW_END, NULL, 0);
+		goto done;
+
+	case PW_FILE_NAME:
+		prev = &pa->se[pa->se_len];
+		next = pw_skip_sep(prev);
+
+		if (*next) {
+			prev = pw_skip_name(next);
+			next_state(pa, PW_FILE_NAME, next, prev - next);
+			break;
+		} else if (next != prev) {
+			next_state(pa, PW_TAIL_SEP, prev, next - prev);
+			break;
+		}
+
+	case PW_TAIL_SEP:
+		next_state(pa, PW_END, NULL, 0);
+	default:
+done:
+		return -1;
+	}
+
+	return 0;
+}
+
+int pw_step_back(struct pathwalk *pa)
+{
+	const xchar *prev;
+	const xchar *next;
+	const xchar *tmp;
+
+	switch (pa->st) {
+	case PW_END:
+		prev = &pa->buf[pa->len - 1];
+		next = pw_skip_sep_back(prev);
+
+		if (next == prev) {
+			next = pw_skip_root_back(prev);
+
+			if (!*next) {
+				next_state(pa, PW_ROOT_NAME,
+					   &next[1], prev - next);
+				break;
+			}
+
+			next = pw_skip_name_back(prev);
+			next_state(pa, PW_FILE_NAME, &next[1], prev - next);
+			break;
+		}
+
+		if (!*next) {
+			next_state(pa, PW_ROOT_DIR, &next[1], prev - next);
+			break;
+		}
+
+		tmp = pw_skip_root_back(next);
+
+		if (!*tmp)
+			next_state(pa, PW_ROOT_DIR, &next[1], prev - next);
+		else
+			next_state(pa, PW_TAIL_SEP, &next[1], prev - next);
+		break;
+
+	case PW_TAIL_SEP:
+		prev = &pa->se[-1];
+		next = pw_skip_name_back(prev);
+
+		next_state(pa, PW_FILE_NAME, &next[1], prev - next);
+		break;
+
+	case PW_FILE_NAME:
+		prev = &pa->se[-1];
+		next = pw_skip_sep_back(prev);
+
+		if (!*next) {
+			if (next == prev) {
+				next_state(pa, PW_PRE, NULL, 0);
+				goto done;
+			}
+
+			next_state(pa, PW_ROOT_DIR, &next[1], prev - next);
+			break;
+		}
+
+		tmp = pw_skip_root_back(next);
+
+		if (*tmp) {
+			prev = next;
+			next = pw_skip_name_back(prev);
+
+			next_state(pa, PW_FILE_NAME, &next[1], prev - next);
+			break;
+		}
+
+		if (next != prev)
+			next_state(pa, PW_ROOT_DIR, &next[1], prev - next);
+		else
+			next_state(pa, PW_ROOT_NAME, &tmp[1], prev - tmp);
+		break;
+
+	case PW_ROOT_DIR:
+		prev = &pa->se[-1];
+		next = &pa->buf[-1];
+
+		if (!*prev) {
+			next_state(pa, PW_PRE, NULL, 0);
+			goto done;
+		}
+
+		next_state(pa, PW_ROOT_NAME, &next[1], prev - next);
+		break;
+
+	case PW_ROOT_NAME:
+		next_state(pa, PW_PRE, NULL, 0);
+	default:
+done:
+		return -1;
+	}
+
+	return 0;
+}
+
+const xchar *pw_to_parent(struct pathwalk *pa)
+{
+	uint len = pa->root_len;
+	enum pathwalk_state st = pa->st;
+
+	pw_step_back(pa);
+
+	if (pa->st == PW_TAIL_SEP)
+		pw_step_back(pa);
+	if (st == PW_END)
+		pw_step_back(pa);
+
+	switch (pa->st) {
+	case PW_FILE_NAME:
+		len = pa->se - pa->buf + pa->se_len;
+		break;
+
+	case PW_ROOT_NAME:
+		len = pa->se - pa->buf + pa->se_len;
+		if (pa->flags & PW_IS_ABS)
+			len += 1;
+		break;
+
+	case PW_ROOT_DIR:
+		len = pa->se - pa->buf + 1;
+		break;
+
+	case PW_PRE:
+		if (!(pa->flags & PW_IS_ABS))
+			return XC(".");
+
+		goto out;
+	}
+
+	if (pa->flags & PW_SHR_RTB) {
+		pa->st = PW_END;
+		pa->len = len;
+	}
+
+out:
+	if (!(pa->flags & PW_SHR_RTB))
+		memcpy(pa->rtb, pa->buf, len * sizeof(*pa->buf));
+
+	pa->rtb[len] = 0;
+	return pa->rtb;
+}
+
+const xchar *pw_basename(struct pathwalk *pa)
+{
+	pa->st = PW_END;
+	pw_step_back(pa);
+
+	if (pa->st == PW_TAIL_SEP) {
+		*(xchar *)pa->se = 0;
+		pw_step_back(pa);
+	}
+
+	if (pa->st == PW_ROOT_DIR) {
+		const xchar *ret = &pa->se[pa->se_len - 1];
+
+		pw_step_back(pa);
+
+		if (pa->st == PW_ROOT_NAME)
+			return pa->buf;
+		return ret;
+	}
+
+	return pa->se;
+}
+
+const xchar *pw_dirname(struct pathwalk *pa)
+{
+	BUG_ON(pa->flags & PW_SHR_RTB);
+
+	enum pathwalk_state st = pa->st;
+	const xchar *se = pa->se;
+
+	const xchar *ret = pw_to_parent(pa);
+
+	pa->st = st;
+	pa->se = se;
+	return ret;
+}

tests/unit/pathwalk.c
@@ -7,7 +7,7 @@
 
 #include <string.h>
 
-#include "pathalt.h"
+#include "pathwalk.h"
 #include "xalloc.h"
 #include "xcf.h"
 
@@ -15,79 +15,79 @@ UT_BEGIN();
 
 UT_ROUTINE(pa_dirname_rel_path)
 {
-	struct pathalt __cleanup(pa_destroy) pa;
+	struct pathwalk __cleanup(pw_destroy) pw;
 	const xchar *dir;
 
-	pa_init(&pa, XC("relative/directory/file"));
+	pw_init(&pw, XC("relative/directory/file"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("relative/directory"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("relative/directory"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("relative/directory"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("relative"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("relative"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("relative"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("."));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("."));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("."));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("."));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("."));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("."));
 }
 
 UT_ROUTINE(pa_dirname_abs_path)
 {
-	struct pathalt __cleanup(pa_destroy) pa;
+	struct pathwalk __cleanup(pw_destroy) pw;
 	const xchar *dir;
 
-	pa_init(&pa, XC("////absolute//directory/file///"));
+	pw_init(&pw, XC("////absolute//directory/file///"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("////absolute//directory"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("////absolute//directory"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("////absolute//directory"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("////absolute"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("////absolute"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("////absolute"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("/"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("/"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("/"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("/"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("/"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("/"));
 }
 
 UT_ROUTINE(pa_dirname_shr_rtb_rel_path)
 {
-	struct pathalt __cleanup(pa_destroy) pa;
+	struct pathwalk __cleanup(pw_destroy) pw;
 	const xchar *dir;
 
 	const xchar *path = XC("relative/directory/file///");
@@ -99,24 +99,24 @@ UT_ROUTINE(pa_dirname_shr_rtb_rel_path)
 
 	buf[0] = 0;
 	memcpy(&buf[1], path, rsz);
-	__pa_init(&pa, &buf[1], len, &buf[1]);
+	__pw_init(&pw, &buf[1], len, &buf[1]);
 
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("relative/directory"));
 
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("relative"));
 
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("."));
 
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("."));
 }
 
 UT_ROUTINE(pa_dirname_shr_rtb_abs_path)
 {
-	struct pathalt __cleanup(pa_destroy) pa;
+	struct pathwalk __cleanup(pw_destroy) pw;
 	const xchar *dir;
 
 	const xchar *path = XC("////absolute//directory/file///");
@@ -128,155 +128,155 @@ UT_ROUTINE(pa_dirname_shr_rtb_abs_path)
 
 	buf[0] = 0;
 	memcpy(&buf[1], path, rsz);
-	__pa_init(&pa, &buf[1], len, &buf[1]);
+	__pw_init(&pw, &buf[1], len, &buf[1]);
 
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("////absolute//directory"));
 
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("////absolute"));
 
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("/"));
 
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("/"));
 }
 
 UT_ROUTINE_WIN32(pa_dirname_rel_path)
 {
-	struct pathalt __cleanup(pa_destroy) pa;
+	struct pathwalk __cleanup(pw_destroy) pw;
 	const xchar *dir;
 
-	pa_init(&pa, XC("C:relative\\directory\\\\file\\\\"));
+	pw_init(&pw, XC("C:relative\\directory\\\\file\\\\"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("C:relative\\directory"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("C:relative\\directory"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("C:relative\\directory"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("C:relative"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("C:relative"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("C:relative"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("C:"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("C:"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("C:"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("."));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("."));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("."));
 }
 
 UT_ROUTINE_WIN32(pa_dirname_abs_path)
 {
-	struct pathalt __cleanup(pa_destroy) pa;
+	struct pathwalk __cleanup(pw_destroy) pw;
 	const xchar *dir;
 
-	pa_init(&pa, XC("C:\\\\\\\\absolute\\\\\\directory\\\\file\\\\"));
+	pw_init(&pw, XC("C:\\\\\\\\absolute\\\\\\directory\\\\file\\\\"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("C:\\\\\\\\absolute\\\\\\directory"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("C:\\\\\\\\absolute\\\\\\directory"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("C:\\\\\\\\absolute\\\\\\directory"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("C:\\\\\\\\absolute"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("C:\\\\\\\\absolute"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("C:\\\\\\\\absolute"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("C:\\"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("C:\\"));
-	dir = pa_to_parent(&pa);
-	UA_STREQ(dir, XC("C:\\"));
-
-	dir = pa_dirname(&pa);
-	UA_STREQ(dir, XC("C:\\"));
-	dir = pa_dirname(&pa);
-	UA_STREQ(dir, XC("C:\\"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("C:\\"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("C:\\"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("C:\\"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
+	UA_STREQ(dir, XC("C:\\"));
+
+	dir = pw_dirname(&pw);
+	UA_STREQ(dir, XC("C:\\"));
+	dir = pw_dirname(&pw);
+	UA_STREQ(dir, XC("C:\\"));
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("C:\\"));
 }
 
 UT_ROUTINE_WIN32(pa_dirname_net_path)
 {
-	struct pathalt __cleanup(pa_destroy) pa;
+	struct pathwalk __cleanup(pw_destroy) pw;
 	const xchar *dir;
 
-	pa_init(&pa, XC("\\\\127.0.0.1\\c$\\path\\file\\\\"));
+	pw_init(&pw, XC("\\\\127.0.0.1\\c$\\path\\file\\\\"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("\\\\127.0.0.1\\c$\\path"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("\\\\127.0.0.1\\c$\\path"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("\\\\127.0.0.1\\c$\\path"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("\\\\127.0.0.1\\c$"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("\\\\127.0.0.1\\c$"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("\\\\127.0.0.1\\c$"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("\\\\127.0.0.1\\"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("\\\\127.0.0.1\\"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("\\\\127.0.0.1\\"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("\\\\127.0.0.1\\"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("\\\\127.0.0.1\\"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("\\\\127.0.0.1\\"));
 }
 
 UT_ROUTINE_WIN32(pa_dirname_drive_path)
 {
-	struct pathalt __cleanup(pa_destroy) pa;
+	struct pathwalk __cleanup(pw_destroy) pw;
 	const xchar *dir;
 
-	pa_init(&pa, XC("c:\\file"));
+	pw_init(&pw, XC("c:\\file"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("c:\\"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("c:\\"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("c:\\"));
 
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("c:\\"));
-	dir = pa_dirname(&pa);
+	dir = pw_dirname(&pw);
 	UA_STREQ(dir, XC("c:\\"));
-	dir = pa_to_parent(&pa);
+	dir = pw_to_parent(&pw);
 	UA_STREQ(dir, XC("c:\\"));
 }
 

unix/pathwalk.c
@@ -3,14 +3,14 @@
  * Copyright 2025 Jiamu Sun <barroit@linux.com>
  */
 
-#include "pathalt.h"
+#include "pathwalk.h"
 
-const xchar *pa_skip_root(const xchar *name)
+const xchar *pw_skip_root(const xchar *name)
 {
 	return name;
 }
 
-const xchar *pa_skip_root_back(const xchar *name)
+const xchar *pw_skip_root_back(const xchar *name)
 {
 	return name;
 }

win32/pathwalk.c
@@ -3,7 +3,7 @@
  * Copyright 2025 Jiamu Sun <barroit@linux.com>
  */
 
-#include "pathalt.h"
+#include "pathwalk.h"
 
 #include "path.h"
 
@@ -38,12 +38,12 @@ static const xchar *skip_drive_back(const xchar *name)
 
 static const xchar *skip_network_root(const xchar *name)
 {
-	const xchar *prev = pa_skip_nsep(name, 2);
+	const xchar *prev = pw_skip_nsep(name, 2);
 
 	if (prev == name)
 		return prev;
 
-	const xchar *next = pa_skip_name(prev);
+	const xchar *next = pw_skip_name(prev);
 
 	if (next == prev)
 		return name;
@@ -53,19 +53,19 @@ static const xchar *skip_network_root(const xchar *name)
 
 static const xchar *skip_network_root_back(const xchar *name)
 {
-	const xchar *prev = pa_skip_name_back(name);
+	const xchar *prev = pw_skip_name_back(name);
 
 	if (prev == name)
 		return prev;
 
-	const xchar *next = pa_skip_nsep_back(prev, 2);
+	const xchar *next = pw_skip_nsep_back(prev, 2);
 
 	if (next == prev)
 		return name;
 	return next;
 }
 
-const xchar *pa_skip_root(const xchar *name)
+const xchar *pw_skip_root(const xchar *name)
 {
 	const xchar *p = skip_drive(name);
 
@@ -75,7 +75,7 @@ const xchar *pa_skip_root(const xchar *name)
 	return skip_network_root(name);
 }
 
-const xchar *pa_skip_root_back(const xchar *name)
+const xchar *pw_skip_root_back(const xchar *name)
 {
 	const xchar *p = skip_network_root_back(name);
 

include/pathalt.h
@@ -1,39 +1,0 @@
-/* SPDX-License-Identifier: GPL-3.0-or-later or MIT */
-/*
- * Copyright 2025 Jiamu Sun <barroit@linux.com>
- */
-
-#ifndef NG39_PATHALT_H
-#define NG39_PATHALT_H
-
-#include "types.h"
-
-#include "pathalt_types.h"
-
-void __pa_init(struct pathalt *pa, xchar *buf, uint len, xchar *rtb);
-
-void pa_init(struct pathalt *pa, const xchar *path);
-
-void pa_destroy(struct pathalt *pa);
-
-const xchar *pa_to_parent(struct pathalt *pa);
-
-const xchar *pa_basename(struct pathalt *pa);
-
-const xchar *pa_dirname(struct pathalt *pa);
-
-int pa_step(struct pathalt *pa);
-
-int pa_step_back(struct pathalt *pa);
-
-const xchar *pa_skip_root(const xchar *name);
-const xchar *pa_skip_sep(const xchar *name);
-const xchar *pa_skip_nsep(const xchar *name, uint n);
-const xchar *pa_skip_name(const xchar *name);
-
-const xchar *pa_skip_root_back(const xchar *name);
-const xchar *pa_skip_sep_back(const xchar *name);
-const xchar *pa_skip_nsep_back(const xchar *name, uint n);
-const xchar *pa_skip_name_back(const xchar *name);
-
-#endif /* NG39_PATHALT_H */

include/pathalt_types.h
@@ -1,47 +1,0 @@
-/* SPDX-License-Identifier: GPL-3.0-or-later or MIT */
-/*
- * Copyright 2025 Jiamu Sun <barroit@linux.com>
- */
-
-#ifndef NG39_PATHALT_TYPES_H
-#define NG39_PATHALT_TYPES_H
-
-#include "types.h"
-
-enum pathalt_state {
-	PA_PRE,
-	PA_ROOT_DIR,
-	PA_ROOT_NAME,
-	PA_FILE_NAME,
-	PA_TAIL_SEP,
-	PA_END,
-};
-
-#define PA_IS_ABS  (1U << 0)
-#define PA_SHR_RTB (1U << 1)
-
-/*
- * Null terminate buffer on both side, otherwise overflow.
- *
- * Some optimizations apply if pathalt->buf and pathalt->rtb point to same
- * address. In that case, pathalt->st becomes unreliable, and don't use:
- *
- *	pa_dirname
- */
-struct pathalt {
-	xchar *buf;
-	uint len;
-
-	const xchar *se;
-	uint se_len;
-
-	xchar *rtb;
-
-	enum pathalt_state st;
-
-	uint root_len;
-
-	u32 flags;
-};
-
-#endif /* NG39_PATHALT_TYPES_H */

include/pathwalk.h
@@ -1,0 +1,39 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later or MIT */
+/*
+ * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ */
+
+#ifndef NG39_PATHWALK_H
+#define NG39_PATHWALK_H
+
+#include "types.h"
+
+#include "pathwalk_types.h"
+
+void __pw_init(struct pathwalk *pa, xchar *buf, uint len, xchar *rtb);
+
+void pw_init(struct pathwalk *pa, const xchar *path);
+
+void pw_destroy(struct pathwalk *pa);
+
+const xchar *pw_to_parent(struct pathwalk *pa);
+
+const xchar *pw_basename(struct pathwalk *pa);
+
+const xchar *pw_dirname(struct pathwalk *pa);
+
+int pw_step(struct pathwalk *pa);
+
+int pw_step_back(struct pathwalk *pa);
+
+const xchar *pw_skip_root(const xchar *name);
+const xchar *pw_skip_sep(const xchar *name);
+const xchar *pw_skip_nsep(const xchar *name, uint n);
+const xchar *pw_skip_name(const xchar *name);
+
+const xchar *pw_skip_root_back(const xchar *name);
+const xchar *pw_skip_sep_back(const xchar *name);
+const xchar *pw_skip_nsep_back(const xchar *name, uint n);
+const xchar *pw_skip_name_back(const xchar *name);
+
+#endif /* NG39_PATHWALK_H */

include/pathwalk_types.h
@@ -1,0 +1,45 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later or MIT */
+/*
+ * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ */
+
+#ifndef NG39_PATHALT_TYPES_H
+#define NG39_PATHALT_TYPES_H
+
+#include "types.h"
+
+enum pathwalk_state {
+	PW_PRE,
+	PW_ROOT_DIR,
+	PW_ROOT_NAME,
+	PW_FILE_NAME,
+	PW_TAIL_SEP,
+	PW_END,
+};
+
+#define PW_IS_ABS  (1U << 0)
+#define PW_SHR_RTB (1U << 1)
+
+/*
+ * Some optimizations apply if pathwalk->buf and pathwalk->rtb point to same
+ * address. In that case, pathwalk->st becomes unreliable, and don't use:
+ *
+ *	pw_dirname
+ */
+struct pathwalk {
+	xchar *buf;
+	uint len;
+
+	const xchar *se;
+	uint se_len;
+
+	xchar *rtb;
+
+	enum pathwalk_state st;
+
+	uint root_len;
+
+	u32 flags;
+};
+
+#endif /* NG39_PATHALT_TYPES_H */

commit c547548875f79385ccf6eb99bf04d3deec5f02b1

pathwalk: fix missing dot assignment to pw->ptb in pw_to_dirname()
Fixes: fea0301c498f ("pathwalk: rename module name to pathwalk")
Signed-off-by: Jiamu Sun <barroit@linux.com>

platfree/pathwalk.c
@@ -310,10 +310,18 @@ const xchar *pw_to_dirname(struct pathwalk *pw)
 		break;
 
 	case PW_PRE:
-		if (!(pw->flags & PW_IS_ABS))
-			return XC(".");
+		if (pw->flags & PW_IS_ABS)
+			goto out;
 
-		goto out;
+		if (pw->rtb == pw->ptb) {
+			pw->st = PW_END;
+			pw->len = 1;
+		}
+
+		pw->rtb[0] = '.';
+		pw->rtb[1] = 0;
+
+		return pw->rtb;
 	}
 
 	if (pw->rtb == pw->ptb) {