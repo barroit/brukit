e6866d18a996469fe8f574ee2b28b1c0132c8fa5
Jiamu Sun <barroit@linux.com>
2025-04-26 15:28:44 +0900

    path: use full platform name in PTH_SEP_*
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- include/path.h
+++ include/path.h
@@ -12,13 +12,13 @@
 #include "attr.h"
 #include "types.h"
 
-#define PTH_SEP_UNI '/'
-#define PTH_SEP_WIN '\\'
+#define PTH_SEP_UNIX  '/'
+#define PTH_SEP_WIN32 '\\'
 
 #if defined(__unix__)
-# define PTH_SEP PTH_SEP_UNI
+# define PTH_SEP PTH_SEP_UNIX
 #elif defined(_WIN32)
-# define PTH_SEP PTH_SEP_WIN
+# define PTH_SEP PTH_SEP_WIN32
 #endif
 
 int chr_is_sep(xchar c);
